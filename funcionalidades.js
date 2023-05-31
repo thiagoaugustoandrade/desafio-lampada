7309e9a5406b20c035e7b874d036e81c9e3f6fbb
Thiago Andrade <132917483+thiagoaugustoandrade@users.noreply.github.com>
2023-05-31 18:49:51 -0300

    lampada concluida

--- funcionalidades.js
+++ funcionalidades.js
@@ -1,5 +1,29 @@
-var lamp = document.getElementById('lampada01.svg')
+var lamp = document.getElementById('lampada')
 
-function ligar(){
-    lamp = 'lampada02.svg'
+function estaQuebrada(){
+    return lamp.src.indexOf('quebrada') > -1
 }
+
+function Ligar(){
+    if(!estaQuebrada()){
+        lamp.src = 'imagens/ligada.svg'
+    }
+}
+
+function Desligar(){
+    if(!estaQuebrada()){
+        lamp.src = 'imagens/desligada.svg'
+    }
+    
+}
+
+
+function Quebrar(){
+    lamp.src = 'imagens/quebrada.svg'
+}
+
+function Trocar(){
+    lamp.src = 'imagens/desligada.svg'
+
+}
+
